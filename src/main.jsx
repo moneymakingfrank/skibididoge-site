91f70dd4f1bdfe71211e8ed4c7aa1837d4999d15
moneymakingfrank <rubio.franklin1@gmail.com>
2025-05-04 12:28:56 -0400

    Update launch date to Juneteenth

--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ function App() {
   }, [])
 
   useEffect(() => {
-    const launchDate = new Date('2025-05-06T12:00:00-04:00')
+    const launchDate = new Date('2025-06-18T12:00:00-04:00')
     const interval = setInterval(() => {
       const now = new Date()
       const diff = launchDate - now
